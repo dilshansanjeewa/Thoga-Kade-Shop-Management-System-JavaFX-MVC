8899b82b9522f9353aecd3eafc5bca1139961a84
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-09-14 22:28:10 +0530

    created thogakade dashboard,controller and add jfoenix dependency

--- src/main/java/controller/DashbordController.java
+++ src/main/java/controller/DashbordController.java
@@ -1,4 +1,161 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class DashbordController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnAddItem;
+
+    @FXML
+    private Button btnCancleOrder;
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private Button btnManageOrder;
+
+    @FXML
+    private Button btnNewCustomer;
+
+    @FXML
+    private Button btnOrderHistory;
+
+    @FXML
+    private Button btnPlaceOrder;
+
+    @FXML
+    private Button btnRefresh;
+
+    @FXML
+    private TableColumn<?, ?> colDescount;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colPackSize;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private ComboBox<?> comboCustId;
+
+    @FXML
+    private ComboBox<?> comboItemCode;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblDescount;
+
+    @FXML
+    private Label lblDescription;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblPackSize;
+
+    @FXML
+    private Label lblQtyOnHand;
+
+    @FXML
+    private Label lblTotal;
+
+    @FXML
+    private Label lblUnitPrice;
+
+    @FXML
+    private TableView<?> tblOrderDetails;
+
+    @FXML
+    private JFXTextField txtDescount;
+
+    @FXML
+    private JFXTextField txtQty;
+
+    @FXML
+    void btnAddItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCancleOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCloseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnManageOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderHistoryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRefreshOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboCustIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboItemCodeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtQtyOnAction(ActionEvent event) {
+
+    }
+
 }
